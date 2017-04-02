293edae4399e098672a6d38112956b75866a5a4f
arifgit12 <arifali.mondal@gmail.com>
2017-04-02 16:10:07 +0300

    spring security library added

--- NearByServiceAPI/src/main/java/com/nearby/app/NearByServiceApiApplication.java
+++ NearByServiceAPI/src/main/java/com/nearby/app/NearByServiceApiApplication.java
@@ -1,12 +1,32 @@
 package com.nearby.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.nearby.app.models.User;
+import com.nearby.app.repository.UserRepository;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class NearByServiceApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NearByServiceApiApplication.class, args);
+	}
+
+	@Bean
+    CommandLineRunner init(final UserRepository accountRepository) {
+
+		return new CommandLineRunner() {
+
+        @Override
+        public void run(String... arg0) throws Exception {
+          //accountRepository.save(new User("arifali", "mondal17"));
+        }
+
+      };
 	}
 }
