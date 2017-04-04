d60155ca81f9be085cf1796e638eeac0b08c0c4c
arifgit12 <arifali.mondal@gmail.com>
2017-04-04 15:18:21 +0300

    users web view

---
+++ NearByServiceAPI/src/main/java/com/nearby/app/controller/UserssController.java
@@ -0,0 +1,43 @@
+package com.nearby.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.nearby.app.models.User;
+import com.nearby.app.services.UserService;
+import com.nearby.app.validator.UserValidator;
+
+@Controller
+public class UserssController {
+
+	@Autowired
+    private UserService userService;
+
+	@Autowired
+    private UserValidator userValidator;
+	@RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+        return "registration";
+    }
+
+	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model) {
+        userValidator.validate(userForm, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+        userService.save(userForm);
+        return "redirect:/welcome";
+    }
+
+	@RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
+    public String welcome(Model model) {
+        return "welcome";
+    }
+}
